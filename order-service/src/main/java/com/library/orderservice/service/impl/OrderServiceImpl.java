689386301368f054c9aae1f41ef0c69d7c4c56cd
Quiti-1556 <Br.quitian@duocuc.cl>
2026-05-17 23:47:14 -0400

    se hizo la respectiva implementacion

--- order-service/src/main/java/com/library/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/library/orderservice/service/impl/OrderServiceImpl.java
@@ -1,4 +1,36 @@
 package com.library.orderservice.service.impl;
 
-public class OrderServiceImpl {
+import com.library.orderservice.dto.OrderRequestDTO;
+import com.library.orderservice.dto.OrderResponseDTO;
+import com.library.orderservice.entity.OrderEntity;
+import com.library.orderservice.repository.OrderRepository;
+import com.library.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+
+    @Override
+    public OrderResponseDTO createOrder(OrderRequestDTO request) {
+
+        OrderEntity order = new OrderEntity();
+
+        order.setUserId(request.getUserId());
+        order.setTotal(request.getTotal());
+        order.setStatus(request.getStatus());
+
+        OrderEntity savedOrder = orderRepository.save(order);
+
+        OrderResponseDTO response = new OrderResponseDTO();
+
+        response.setId(savedOrder.getId());
+        response.setUserId(savedOrder.getUserId());
+        response.setTotal(savedOrder.getTotal());
+        response.setStatus(savedOrder.getStatus());
+
+        return response;
+    }
 }
