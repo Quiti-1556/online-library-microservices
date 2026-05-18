55b56df26d9c133c3726126cb5f08bf70e1fa4f5
Quiti-1556 <Br.quitian@duocuc.cl>
2026-05-18 17:02:08 -0400

    revisamos si nos daba el token en postman

--- auth-service/src/main/java/com/library/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/library/authservice/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.library.authservice.controller;
 
 
+import com.library.authservice.dto.LoginRequestDTO;
+import com.library.authservice.dto.LoginResponseDTO;
 import com.library.authservice.dto.RegisterRequestDTO;
 import com.library.authservice.dto.UserResponseDTO;
+import com.library.authservice.security.JwtService;
 import com.library.authservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO request) {
+
+        String token = jwtService.generateToken(request.getEmail());
+
+        return new LoginResponseDTO(token);
+    }
 
     @PostMapping("/register")
     public UserResponseDTO registerUser(@RequestBody RegisterRequestDTO request) {
