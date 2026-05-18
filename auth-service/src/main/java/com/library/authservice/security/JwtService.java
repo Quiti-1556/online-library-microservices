e6109c0e52bc2e5b230b8c9d2ed120c14abeb7ee
Quiti-1556 <Br.quitian@duocuc.cl>
2026-05-18 16:42:38 -0400

    agregue el jwt para el token y autenticacion

---
+++ auth-service/src/main/java/com/library/authservice/security/JwtService.java
@@ -0,0 +1,36 @@
+package com.library.authservice.security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "mysecretkeymysecretkeymysecretkey";
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+}
--- auth-service/src/main/java/com/library/authservice/security/SecurityConfig.java
+++ auth-service/src/main/java/com/library/authservice/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.library.authservice.config;
+package com.library.authservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
