977030cdfeeb866c299b07042383f13a32733fc8
Vania Bustos Fuentealba <f.bustos@duocuc.cl>
2026-06-19 04:04:21 -0400

    Cambios en el servicio de payment-service

--- payment-service/src/main/java/com/library/paymentservice/entity/Payment.java
+++ payment-service/src/main/java/com/library/paymentservice/entity/Payment.java
@@ -5,18 +5,22 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="payments")
+@Table(name = "payments")
 public class Payment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Long orderId;
 
+    @Column(nullable = false)
     private Double amount;
 
+    @Column(nullable = false, length = 50)
     private String paymentMethod;
 
+    @Column(nullable = false, length = 50)
     private String status;
-
 }
