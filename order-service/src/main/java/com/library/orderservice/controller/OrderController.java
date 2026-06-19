0e6c4ebd2aac179f8cc9a89e55a8ba23346f939d
Vania Bustos Fuentealba <f.bustos@duocuc.cl>
2026-06-19 03:22:31 -0400

    Cambios en el servicio de order-service

--- order-service/src/main/java/com/library/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/library/orderservice/controller/OrderController.java
@@ -3,22 +3,26 @@ package com.library.orderservice.controller;
 import com.library.orderservice.dto.OrderRequestDTO;
 import com.library.orderservice.dto.OrderResponseDTO;
 import com.library.orderservice.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     private final OrderService orderService;
 
     @PostMapping
-    public OrderResponseDTO createOrder(@RequestBody OrderRequestDTO request) {
-
-        return orderService.createOrder(request);
+    public ResponseEntity<OrderResponseDTO> createOrder(@Valid @RequestBody OrderRequestDTO request) {
+        log.info("Creando orden para userId {} y bookId {}", request.getUserId(), request.getBookId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(request));
     }
-
 }
--- order-service/src/main/java/com/library/orderservice/dto/OrderRequestDTO.java
+++ order-service/src/main/java/com/library/orderservice/dto/OrderRequestDTO.java
@@ -5,8 +5,7 @@ import lombok.Data;
 @Data
 public class OrderRequestDTO {
     private Long userId;
-
     private Double total;
-
     private String status;
+    private Long bookId;
 }
