8986dd543d8772ea477c6004d4a1a9aa8a672221
Quiti-1556 <br.quitian@duocuc.cl>
2026-06-23 19:55:28 -0400

    Add Swagger documentation to auth-service

--- cart-service/src/main/java/com/library/cartservice/config/FlywayConfig.java
+++ cart-service/src/main/java/com/library/cartservice/config/FlywayConfig.java
@@ -10,16 +10,16 @@ import javax.sql.DataSource;
 @Configuration
 public class FlywayConfig {
 
-    //@Bean
-    //public CommandLineRunner migrateDatabase(DataSource dataSource) {
-        //return args -> {
-            //Flyway flyway = Flyway.configure()
-                    //.dataSource(dataSource)
-                    //.locations("classpath:db/migration")
-                    //.load();
+    @Bean
+    public CommandLineRunner migrateDatabase(DataSource dataSource) {
+        return args -> {
+            Flyway flyway = Flyway.configure()
+                    .dataSource(dataSource)
+                    .locations("classpath:db/migration")
+                    .load();
 
-            //flyway.migrate();
-            //System.out.println("Flyway ejecutado correctamente");
-        //};
-    //}
+            flyway.migrate();
+            System.out.println("Flyway ejecutado correctamente");
+        };
+    }
 }
